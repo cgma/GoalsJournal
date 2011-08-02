9750d5df57924a25c30d6682eee8cb074a40513e
Carlos Marin <ing.cma@gmail.com>
2011-08-02 01:51:45 -0500

    Comment update

--- src/jmydays/JMyDays.java
+++ src/jmydays/JMyDays.java
@@ -1,10 +1,24 @@
 /**
  * GoalsJournal (before: JMyDays) is a Daily habit tracker inspired on www.joesgoals.com
  * No warranty expressed or implied. Use at your own risk.
- * The project is licensed under GNU GPL.
  *
  * @author Carlos G. Marin
  * @contact ing.cma AT gmail DOT com
+ *
+ *   Copyright 2011 Carlos Marin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
 
 /*
